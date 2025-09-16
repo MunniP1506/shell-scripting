e4c57d9cff8b579ff73a6ee556dcd13c94188ddd
Munni Pulacharla <munnip1506@gmail.com>
2025-09-16 08:24:32 +0530

    added conversation script

---
+++ repos/2.Conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh how are you?"
+echo "suresh:: Hi Ramesh, i am fine, how are you doing?"
+echo "Ramesh:: i am doing good suresh.what are you doing?"
+echo "Suresh:: I am going to start learning shellscript ramesh"
