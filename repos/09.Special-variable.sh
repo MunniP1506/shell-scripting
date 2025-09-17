8f3b113af549dc23ae0b8740b1f807e8b5c573e0
Munni Pulacharla <munnip1506@gmail.com>
2025-09-17 12:10:59 +0530

    special comment script is added

--- repos/09.Special-variable.sh
+++ repos/09.Special-variable.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "All the variables passed to the script: $@
-
+echo "All the variables passed to the script: $@"
+echo "Number of variable passed: $#"
+echo "script name : $0"
+echo "Current working directory: $PWD"
+echo "Home directory of the current user: $HOME"
