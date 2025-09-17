a5f0dd8fc56bc405835403e87dd2748b4b452b49
Munni Pulacharla <munnip1506@gmail.com>
2025-09-17 12:14:58 +0530

    variable script is added

---
+++ repos/04.variable.sh
@@ -0,0 +1,8 @@
+#1/bin/bash
+Person=$!
+Person=$2
+
+cho "$Person1:: Hi $Person2 how are you?"
+echo "$Person2:: Hi $Person1, i am fine, how are you doing?"
+echo "$Person1:: i am doing good $Person2.what are you doing?"
+echo "$Person2:: I am going to start learning shellscript $Person1"
