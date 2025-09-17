01970d795e46f87cdb1a57dfa1aeef720e8eee40
Munni Pulacharla <munnip1506@gmail.com>
2025-09-17 12:25:19 +0530

    array script is added

---
+++ repos/06.array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# index start from 0, size is 3
+Fruits=("Apple" "Kiwi" "mango") #Array
+
+echo "First fruit is: ${Fruit[0]}"
+echo "Second fruit is: ${Fruit[1]}"
+echo "Third fruit is: ${Fruit[2]}"
+
+echo "First fruit is :${Fruit[@]}"
